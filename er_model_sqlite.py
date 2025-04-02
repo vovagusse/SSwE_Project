3d5629a3e10b0805dd75871f40ed059499266bb5
Artyom <artemrule@gmail.com>
2025-04-02 12:40:19 +0300

    ER model using Peewee (planning to use SQLAlchemy instead)

--- er_model_sqlite.py
+++ er_model_sqlite.py
@@ -1,6 +1,7 @@
 from peewee import *
 from app import MY_DB
-
+# from . import db
+import datetime
 
 db=SqliteDatabase(MY_DB)
 
@@ -20,8 +21,64 @@ class Image(Model):
     image_id = AutoField()
     image_uri = TextField(
         default="https://via.placeholder.com/400x300")
-    #primary key of of product is product_id
     id_product = ForeignKeyField(
         Product, backref="product_id") 
     class Meta:
         database = db
+
+class File(Model):
+    file_id = AutoField()
+    file_uri = TextField()
+    id_product = ForeignKeyField(
+        Product, backref="product_id") 
+    class Meta:
+        database = db
+
+
+class Video(Model):
+    video_id = AutoField()
+    video_uri = TextField(
+        default="https://via.placeholder.com/400x300")
+    id_product = ForeignKeyField(
+        Product, backref="product_id") 
+    class Meta:
+        database = db
+
+
+class User(Model):
+    user_id = AutoField()
+    full_name = TextField() #Полное ФИО
+    username = TextField() #Псевдоним или отображаемое другим пользователям имя
+    mail = TextField() #Адрес почты (Логин)
+    password = TextField() #Пароль
+    class Meta:
+        database = db
+
+
+class Developer(Model):
+    developer_id = AutoField()
+    id_user = ForeignKeyField(
+        User, backref="user_id") 
+    class Meta:
+        database = db
+
+
+class AuthSession(Model):
+    auth_id = AutoField()
+    id_user = ForeignKeyField(
+        User, backref="user_id") 
+    class Meta:
+        database = db
+
+
+class Purchased(Model):
+    purchase_id = AutoField()
+    id_user = ForeignKeyField(
+        User, backref="user_id")
+    id_product = ForeignKeyField(
+        Product, backref="product_id") 
+    time_of_purchase = DateTimeField(default=datetime.datetime.now())
+    cost_of_purchase = FloatField()
+    class Meta:
+        database = db
+    
